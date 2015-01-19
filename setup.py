6fac94958b12b4947cdee88908de45a910f2deb3
nazrulworld <connect2nazrul@gmail.com>
2015-01-20 00:14:11 +0600

    setup py updated

--- setup.py
+++ setup.py
@@ -1,3 +1,30 @@
 # _*_ coding: utf-8 _*_
-__author__ = 'nislam <connect2nazrul@gmail.com>'
-from setuptools import setup
+
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='nz.plone',
+    version='1.dev0',
+    url='http://pypi.python.org/pypi/nz.plone',
+    description="Plone Management Utility by Nazrul",
+    author="Md. Nazrul Islam",
+    author_email="connect2nazrul@gmail.com",
+    long_description=None,
+    packages=["nz", "nz.plone", "nz.plone.install"],
+    package_dir={"": "src"},
+    namespace_packages=["nz"],
+    include_package_data=True,
+    install_requires=["setuptools", "six"],
+    zip_safe=False,
+    entry_point={
+        "console_scripts": {
+            "plone": ["nz.plone.cli:main"]
+        }
+    }
+)
+
